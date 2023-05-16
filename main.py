59fe7344c3bbdd5603841954470dbdf52b4d5ee5
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-16 19:51:46 +0200

    Refactor the code

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from tkinter import *
+from tkinter import Tk, Label, Entry, Button
+
+def convert_miles_to_km():
+    miles = float(mile_input.get())
+    kilometers = round(miles * 1.60934, 2)
+    result2.config(text=kilometers)
 
 window = Tk()
-
 window.title("Mile to KM Converter")
-
 window.minsize(width=500, height=300)
 window.config(padx=60, pady=40)
 
@@ -13,15 +16,17 @@ mile_input.grid(column=1, row=0)
 mile_label = Label(text="Miles")
 mile_label.grid(column=3, row=0)
 
-def converter():
-    result_km = round((float(mile_input.get()) * 1.60934), 2)
-    result2.config(text=result_km)
+result1 = Label(text="Is equal to")
+result1.grid(column=0, row=1)
 
-
-result1 = Label(text="Is equal to").grid(column=0, row=1)
-result2 = Label(text=0)
+result2 = Label(text="")
 result2.grid(column=1, row=1, ipadx=10, ipady=10)
-text1 = Label(text="km").grid(column=3, row=1, ipadx=10, ipady=10)
-button_km = Button(text="Convert", command=converter).grid(column=1, row=3)
+
+text1 = Label(text="km")
+text1.grid(column=3, row=1, ipadx=10, ipady=10)
+
+button_km = Button(text="Convert", command=convert_miles_to_km)
+button_km.grid(column=1, row=3)
 
 window.mainloop()
+
